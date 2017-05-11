b382f060c616a9a8a4bfdd08a6cce027298e25a5
Sklif79 <sklif_@mail.ru>
2017-05-11 21:10:19 +0300

    11.05 work

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -4,22 +4,39 @@ $(document).ready(function () {
         $('form.search-form').toggleClass('show-search-form');
     });
 
+    // $('a.catalog-item-hidden__el-lnk').hover(
+    //     function () {
+    //         $(this).next('.catalog-item-hidden__more').show();
+    //     },
+        // function () {
+        // if ($('a.catalog-item-hidden__el-lnk').mouseenter != this) {
+        //     $(this).next('.catalog-item-hidden__more').hide();
+        // }
+        // });
+
+    $('a.catalog-item-hidden__el-lnk').mouseenter(
+        function () {
+
+            // console.log(hideEl);
+            $('a.catalog-item-hidden__el-lnk').next('.catalog-item-hidden__more').hide();
+            $(this).next('.catalog-item-hidden__more').show();
+            var hideEl = $(this).next('.catalog-item-hidden__more').outerHeight() + 25;
+            $(this).next('.catalog-item-hidden__more').css({'top':'-' + hideEl + 'px'});
+            console.log(hideEl);
+        }
+    );
+
+    $('div.catalog-item, .catalog-item-hidden__more').mouseleave(
+        function () {
+            $('a.catalog-item-hidden__el-lnk').next('.catalog-item-hidden__more').hide();
+        }
+    );
+
+    // if ($('.catalog-item-hidden__el').is(':visible')) {
+    //     console.log('dfdfdf')
+    // }
+
     buttonUp();
-
-    //анимашки социальных кнопок
-    // $('.sc-btns__el').hover(function () {
-    //     $(this).children('.sc-btns__el-hidden-line').show('fast');
-    //     $(this).children('.sc-btns__el-hidden').show('fast');
-    //
-    // }, function () {
-    //     $(this).children('.sc-btns__el-hidden-line').hide('fast');
-    //     $(this).children('.sc-btns__el-hidden').hide('fast');
-    // });
-
-
-    // validatePopups();
-
-    // $('.expo__item-title').setMaxHeights();
 
 
     // $('a.modalbox').fancybox({
@@ -103,145 +120,5 @@ function buttonUp() {
 
     $('#toTop').click(function () {
         $('html, body').animate({scrollTop: 0}, 500);
-    });
-}
-
-
-//плагин обрезки текста
-$(document).ready(function () {
-    //плагин
-    jQuery.fn.liTextLength = function (options) {
-        // настройки по умолчанию
-        var o = jQuery.extend({
-            length: 150,                                    //Видимое кол-во символов
-            afterLength: '...',                                //Текст после видимого содержания
-            fullText: true,                                    //Добавить ссылку для отображения скрытого текста
-            moreText: '<br>полный&nbsp;текст',                //Текст ссылки до показа скрытого содержания
-            lessText: '<br>скрыть&nbsp;полный&nbsp;текст'    //Текст ссылки после показа скрытого содержания
-        }, options);
-        return this.each(function () {
-            var
-                $el = $(this),
-                elText = $.trim($el.text()),
-                elLength = elText.length;
-            if (elLength > o.length) {
-                var
-                    textSlice = $.trim(elText.substr(0, o.length)),
-                    textSliced = $.trim(elText.substr(o.length));
-                if (textSlice.length < o.length) {
-                    var
-                        textVisible = textSlice,
-                        textHidden = $.trim(elText.substr(o.length));
-                } else {
-                    var
-                        arrSlice = textSlice.split(' '),
-                        popped = arrSlice.pop(),
-                        textVisible = arrSlice.join(' ') + ' ',
-                        textHidden = popped + textSliced + ' ';
-                }
-                ;
-                var
-                    $elTextHidden = $('<span>').addClass('elTextHidden').html(textHidden),
-                    $afterLength = $('<span>').addClass('afterLength').html(o.afterLength + ' '),
-                    $more = $('<span>').addClass('more').html(o.moreText);
-                $el.text(textVisible).append($afterLength).append($elTextHidden);
-                var displayStyle = $elTextHidden.css('display');
-                $elTextHidden.hide();
-                if (o.fullText) {
-                    $el.append($more);
-                    $more.click(function () {
-                        if ($elTextHidden.is(':hidden')) {
-                            $elTextHidden.css({display: displayStyle});
-                            $more.html(o.lessText);
-                            $afterLength.hide();
-                        } else {
-                            $elTextHidden.hide();
-                            $more.html(o.moreText);
-                            $afterLength.show();
-                        }
-                        ;
-                        return false;
-                    });
-                } else {
-                    $elTextHidden.remove();
-                }
-                ;
-            }
-            ;
-        });
-    };
-
-    // if ($(window).width() < 497) {
-    //     //инициализация
-    //     $('#tab-description').liTextLength({
-    //         length: 597,                                    //Видимое кол-во символов
-    //         afterLength: '...',                                //Текст после видимого содержания
-    //         fullText: true,                                    //Добавить ссылку для отображения скрытого текста
-    //         moreText: '<div class="description-more"><span>Подробнее</span></div>',                //Текст ссылки до показа скрытого содержания
-    //         lessText: '<div class="description-more"><span>Свернуть</span></div>'    //Текст ссылки после показа скрытого содержания
-    //     });
-    // }
-
-});
-
-
-$.fn.setMaxHeights = function () {
-    var maxHeight = this.map(function (i, e) {
-        return $(e).height();
-    }).get();
-
-    return this.height(Math.max.apply(this, maxHeight));
-};
-
-//валидатор формы попап
-function validatePopups() {
-    var myLanguage = {
-        errorTitle: "Ошибка отправки формы!",
-        requiredField: "Это обязательное поле",
-        requiredFields: "Вы задали не все обязательные поля",
-        badTime: "Вы задали некорректное время",
-        badEmail: "Вы задали некорректный e-mail",
-        badTelephone: "Вы задали некорректный номер телефона",
-        badSecurityAnswer: "Вы задали некорректный ответ на секретный вопрос",
-        badDate: "Вы задали некорректную дату",
-        lengthBadStart: "Значение должно быть в диапазоне",
-        lengthBadEnd: " символов",
-        lengthTooLongStart: "Значение длинее, чем ",
-        lengthTooShortStart: "Значение меньше, чем ",
-        notConfirmed: "Введённые значения не могут быть подтверждены",
-        badDomain: "Некорректное значение домена",
-        badUrl: "Некорретный URL",
-        badCustomVal: "Введённое значение неверно",
-        andSpaces: " и пробелы ",
-        badInt: "Значение - не число",
-        badSecurityNumber: "Введённый защитный номер - неправильный",
-        badUKVatAnswer: "Некорректный UK VAT номер",
-        badStrength: "Пароль не достаточно надёжен",
-        badNumberOfSelectedOptionsStart: "Вы должны выбрать как минимум ",
-        badNumberOfSelectedOptionsEnd: " ответов",
-        badAlphaNumeric: "Значение должно содержать только числа и буквы ",
-        badAlphaNumericExtra: " и ",
-        wrongFileSize: "Загружаемый файл слишком велик (максимальный размер %s)",
-        wrongFileType: "Принимаются файлы следующих типов %s",
-        groupCheckedRangeStart: "Выберите между ",
-        groupCheckedTooFewStart: "Выберите как минимум ",
-        groupCheckedTooManyStart: "Выберите максимум из ",
-        groupCheckedEnd: " элемент(ов)",
-        badCreditCard: "Номер кредитной карты некорректен",
-        badCVV: "CVV номер некорректно",
-        wrongFileDim: "Неверные размеры графического файла,",
-        imageTooTall: "изображение не может быть уже чем",
-        imageTooWide: "изображение не может быть шире чем",
-        imageTooSmall: "изображение слишком мало",
-        min: "минимум",
-        max: "максимум",
-        imageRatioNotAccepted: "Изображение с таким соотношением сторон не принимается",
-        badBrazilTelephoneAnswer: "Введённый номер телефона неправильный",
-        badBrazilCEPAnswer: "CEP неправильный",
-        badBrazilCPFAnswer: "CPF неправильный"
-    };
-
-    $.validate({
-        language: myLanguage
     });
 }
